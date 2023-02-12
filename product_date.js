f10fada2b2207a8ed1d4d8804ba1a9935606693d
luka <124693735+lukzazashvili@users.noreply.github.com>
2023-02-12 21:35:15 +0400

    Add files via upload

---
+++ product_date.js
@@ -0,0 +1,64 @@
+const product_card = [
+    {
+        id:1,
+        product_name: "მამაკაცის შორტი",
+        description: "Hands-free",
+        price: 35,
+        currency:"$",
+        thumb: "./images/1.png"
+    },
+    {
+        id:2,
+        product_name: "ბიჭების მაიკა",
+        description: "hands-free, Hads-on Musch experience",
+        price: 20,
+        currency: "$",
+        thumb: "./images/2.png"
+    },
+    {
+       id:1,
+       product_name: "ბიჭების მაიკა",
+       description:"hands-free Hads-on Musch experience",
+       price: 14,
+       currency:"$",
+       thumb: "./images/3.png"
+       
+    },
+    {
+        id:1,
+        product_name:"თეთრი მაიკა",
+        description:"hands-free Hads-on Musch experience",
+        price: 12,
+        currency:"$",
+        thumb: "./images/4.png"
+        
+     },
+     {
+        id:1,
+        product_name:"გოგოების მაიკა",
+        description:"hands-free Hads-on Musch experience",
+        price: 30,
+        currency:"$",
+        thumb: "./images/5.png"
+        
+     },
+     {
+        id:2,
+        product_name:"ბავშვების მაიკა ",
+        description:"hands-free Hads-on Musch experience",
+        price: 30,
+        currency:"$",
+        thumb: "./images/6.png"
+        
+    },
+    {
+      id:1,
+      product_name:"გოგოს მაიკა",
+      description:"Hands-free, hads-on Musch Experience",
+      price: 40,
+      currency: "$",
+      thumb:"./images/7.png"
+
+    }
+]
+export default product_card;
